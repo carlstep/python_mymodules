3c27a4bdb1d0eb9bd4d18582c92c3502b66cf8db
carlstep <carlstep333@me.com>
2021-03-05 21:58:52 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch2',
+    version='1.0',
+    description='the head first python search tools',
+    author='CS HF Python 2e',
+    author_email='carlstep333@me.com',
+    url='',
+    py_modules=['vsearch2'],
+    )
